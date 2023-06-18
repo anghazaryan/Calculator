91792182b4fee56ab7aefd7351ed1ed20253922e
anghazaryan <anahit.ghazaryan91@gmail.com>
2023-06-18 22:42:25 +0400

    commit calculator

---
+++ Calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner inputField = new Scanner(System.in);
+		System.out.println("What is your name");
+
+		String firstName = inputField.nextLine();
+		        System.out.println(firstName + " please enter any number");
+
+		int firstNumber = inputField.nextInt();
+				System.out.println("Please enter another number ");	
+
+		int secondNumber = inputField.nextInt();
+
+		System.out.println("Choose an operator: +, -, *, or /");
+		 char operator = inputField.next().charAt(0);
+
+				int result = 0;
+
+	switch (operator) {
+
+	case '+': 
+		result = firstNumber + secondNumber;
+		System.out.println(firstNumber + " + " + secondNumber + " = " + result);
+		break;
+
+	case '-':
+		result = firstNumber - secondNumber;
+		System.out.println(firstNumber + " - " + secondNumber + " = " + result);
+		break;
+
+	case '*':
+		result = firstNumber * secondNumber;
+		System.out.println(firstNumber + " * " + secondNumber + " = " + result);
+		break;
+
+	case '/':
+		result = firstNumber / secondNumber;
+		System.out.println(firstNumber + " / " + secondNumber + " = "+ result);
+		break;
+
+	default:
+		System.out.println("Wrong value is inserted!");
+		break;
+	  }
+    }
+  }
